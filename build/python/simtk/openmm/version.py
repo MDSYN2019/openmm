2bbc49f42cd646fb9ed0b463c60a53ac379a37c2
Sang <gnatnoh123@googlemail.com>
2019-04-03 18:45:01 +0100

    new

---
+++ build/python/simtk/openmm/version.py
@@ -0,0 +1,1 @@
+git_revision = 'a770038f125588ab0cced8cd67f04e5083f24e0d'
---
+++ build/wrappers/python/pysetupbdist.cmake
@@ -0,0 +1,8 @@
+set(ENV{OPENMM_INCLUDE_PATH} "/home/oohnohnoh1/Desktop/GIT/openmm/./include;/home/oohnohnoh1/Desktop/GIT/openmm/./include/openmm;/home/oohnohnoh1/Desktop/GIT/openmm/./include/openmm/internal;/home/oohnohnoh1/Desktop/GIT/openmm/openmmapi/include;/home/oohnohnoh1/Desktop/GIT/openmm/openmmapi/include/openmm;/home/oohnohnoh1/Desktop/GIT/openmm/openmmapi/include/openmm/internal;/home/oohnohnoh1/Desktop/GIT/openmm/olla/include;/home/oohnohnoh1/Desktop/GIT/openmm/olla/include/openmm;/home/oohnohnoh1/Desktop/GIT/openmm/olla/include/openmm/internal;/home/oohnohnoh1/Desktop/GIT/openmm/serialization/include;/home/oohnohnoh1/Desktop/GIT/openmm/serialization/include/openmm;/home/oohnohnoh1/Desktop/GIT/openmm/serialization/include/openmm/internal;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/amoeba/openmmapi/include;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/amoeba/openmmapi/include/openmm;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/amoeba/openmmapi/include/openmm/internal;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/rpmd/openmmapi/include;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/rpmd/openmmapi/include/openmm;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/rpmd/openmmapi/include/openmm/internal;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/drude/openmmapi/include;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/drude/openmmapi/include/openmm;/home/oohnohnoh1/Desktop/GIT/openmm/plugins/drude/openmmapi/include/openmm/internal")
+set(ENV{OPENMM_LIB_PATH} "/usr/local/openmm/lib")
+message("OPENMM_LIB_PATH = " $ENV{OPENMM_LIB_PATH})
+message("OPENMM_INCLUDE_PATH = " $ENV{OPENMM_INCLUDE_PATH})
+execute_process(
+    COMMAND "/home/oohnohnoh1/anaconda3/bin/python" setup.py bdist
+    WORKING_DIRECTORY ""
+)
